0c4b2fc011a8f110da20e24803810dd5d69e3b5c
ewazna <e.m.wazna@gmail.com>
2026-03-08 17:34:56 +0100

    Add transforming to CSV file

--- src/candidates/controller/candidates.controller.ts
+++ src/candidates/controller/candidates.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { CandidatesService } from '../service/candidates.service';
-import { CandidatesJobApplications } from '../models/candidatesJobApplications';
+import type { Response } from 'express';
 
 @Controller('candidates')
 export class CandidatesController {
   constructor(private readonly candidatesService: CandidatesService) {}
 
   @Get()
-  async getAllCandidatesData(): Promise<CandidatesJobApplications> {
-    return this.candidatesService.getAllCandidatesData();
+  async getAllCandidatesData(@Res() response: Response) {
+    const csvFileRows = await this.candidatesService.getAllCandidatesData();
+
+    response.setHeader('Content-Type', 'text/csv');
+    response.setHeader(
+      'Content-Disposition',
+      'attachment; filename="users.csv"',
+    );
+
+    response.send(csvFileRows);
   }
 }
--- src/candidates/service/candidates.service.ts
+++ src/candidates/service/candidates.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CandidatesRepository } from '../repository/candidates.repository';
-import { CandidatesJobApplications } from '../models/candidatesJobApplications';
 
 @Injectable()
 export class CandidatesService {
   constructor(private readonly candidatesRepository: CandidatesRepository) {}
 
-  async getAllCandidatesData(): Promise<CandidatesJobApplications> {
-    return this.candidatesRepository.getAllCandidates();
+  async getAllCandidatesData(): Promise<string> {
+    const { candidates, jobApplications } =
+      await this.candidatesRepository.getAllCandidates();
+
+    const jobApplicationsMap: Map<string, string> = new Map();
+
+    jobApplications.forEach((jobApplication) =>
+      jobApplicationsMap.set(
+        jobApplication.id,
+        jobApplication.attributes['created-at'],
+      ),
+    );
+
+    const csvFileData = candidates.flatMap((candidate) => {
+      return candidate.relationships['job-applications'].data.map(
+        ({ id: jobId }) => {
+          return {
+            candidate_id: candidate.id,
+            first_name: candidate.attributes['first-name'] || '',
+            last_name: candidate.attributes['last-name'] || '',
+            email: candidate.attributes.email || '',
+            job_application_id: jobId || '',
+            job_application_created_at:
+              jobApplicationsMap.get(`${jobId}`) || '',
+          };
+        },
+      );
+    });
+
+    const header = Object.keys(csvFileData[0]).join(',') + '\n';
+
+    return (
+      header +
+      csvFileData.map((data) => Object.values(data).join(',')).join('\n')
+    );
   }
 }
